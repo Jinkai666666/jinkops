2617b835ac8f1a13157d5e2d6fc071d290697662
Jinkai+666666 <2318556046@qq.com>
2025-11-06 23:17:01 +0800

    日志模块倒序查询 + Knife4j 文档集成

--- src/main/java/com/jinkops/service/OperationLogService.java
+++ src/main/java/com/jinkops/service/OperationLogService.java
@@ -2,11 +2,13 @@ package com.jinkops.service;
 import com.jinkops.entity.OperationLogEntity;
 import com.jinkops.repository.OperationLogRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
-//操作日志服务接口
+//操作日志服务类
 @Service
 public class OperationLogService {
    private final OperationLogRepository operationLogRepository;
@@ -18,7 +20,12 @@ public class OperationLogService {
 
     // 分页获取全部日志
     public Page<OperationLogEntity> getLogs(Pageable pageable){
-        return operationLogRepository.findAll(pageable);
+        Pageable sortedPageable = PageRequest.of(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                Sort.by(Sort.Direction.DESC, "timestamp") // 按 createTime 倒序
+        );
+        return operationLogRepository.findAll(sortedPageable);
     }
 
     // 模糊搜索日志（可按用户名或操作名称模糊匹配）
@@ -30,4 +37,7 @@ public class OperationLogService {
         // 有关键字时执行模糊搜索
         return operationLogRepository.searchLogs(keyword, pageable);
     }
+
+
+
 }
