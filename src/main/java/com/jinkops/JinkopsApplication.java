85f5b017f2d435f123feb07da5f18a194777483d
Jinkai+666666 <2318556046@qq.com>
2026-02-06 00:38:09 +0800

    解決 mq沒有被實時監聽 未被消費的問題

--- src/main/java/com/jinkops/JinkopsApplication.java
+++ src/main/java/com/jinkops/JinkopsApplication.java
@@ -2,10 +2,12 @@ package com.jinkops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
+@EnableRabbit
 @SpringBootApplication
 public class JinkopsApplication {
     public static void main(String[] args) {
